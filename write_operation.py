bf9dc592466f8fa441f218b1a9fc10b8822fb3f0
nijuval <nijuvalb.com>
2026-09-19 11:28:27 +0530

    add budget

--- write_operation.py
+++ write_operation.py
@@ -3,3 +3,8 @@ def add_category(path,category):
     data= data_load.import_data(path)
     data["categories"].append(category)
     data_load.write_data(data,path)
+
+def add_budget(path,budget_name,budget):
+    data=data_load.import_data(path)
+    data["monthlyBudget"][budget_name] = budget
+    data_load.write_data(data,path)
